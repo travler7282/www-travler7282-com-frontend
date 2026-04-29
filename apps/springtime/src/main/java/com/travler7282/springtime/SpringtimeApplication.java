a731007af2b5248fd7d6d933bc2f9cbf04824cca
Michael Hunt <travler7282@comcast.net>
2026-04-28 23:59:59 -0400

    Added springtime app (Spring Boot)

---
+++ apps/springtime/src/main/java/com/travler7282/springtime/SpringtimeApplication.java
@@ -0,0 +1,13 @@
+package com.travler7282.springtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtimeApplication.class, args);
+	}
+
+}
---
+++ apps/springtime/src/test/java/com/travler7282/springtime/SpringtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travler7282.springtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
